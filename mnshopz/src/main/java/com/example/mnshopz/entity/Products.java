6b123fdb9edf9ffad868eec24b716a40152713fa
Enamul Hussain Manjur <enamulmanjur@mpura.com>
2023-03-28 17:35:39 +0530

    created script for order histroy page

--- mnshopz/src/main/java/com/example/mnshopz/entity/Products.java
+++ mnshopz/src/main/java/com/example/mnshopz/entity/Products.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Entity
